47d2ee871e992eecc9bed77dbb8495c6243e89bf
Bandaranayake97 <2019e021@eng.jfn.ac.lk>
2023-12-10 10:18:30 +0530

    intro add job list

--- job-finding-app/components/Intro.jsx
+++ job-finding-app/components/Intro.jsx
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import JobsCard from './JobsCard';
 import jobimage from './aserts/job.jpg';
 import { AiOutlineCaretDown,AiOutlineCaretUp } from 'react-icons/ai';
+import Link from 'next/link';
 export default function Intro() {
   const [search, setSearch] = useState('');
   const jobData = useSelector(state => state.Job.JobData);
@@ -13,8 +14,11 @@ export default function Intro() {
   const [doneSearch , setDoneSearch] = useState(false)
   const [isOpen,setIsOpen] = useState(false);
   const [isOpen1,setIsOpen1] = useState(false);
-
-
+  const [isOpen2,setIsOpen2] = useState(false);
+  const [isOpen3,setIsOpen3] = useState(false);
+  const [isOpen4,setIsOpen4] = useState(false);
+  const [isOpen5,setIsOpen5] = useState(false);
+  const [isOpen6,setIsOpen6] = useState(false);
   const handleSearch = (e) => {
     e.preventDefault();
     const filteredJobs = jobData?.filter((job) => {
@@ -27,9 +31,122 @@ export default function Intro() {
 
   return (
     <>
-      <div className='w-full  h-full flex items-center lg:justify-start py-36 justify-center flex-wrap  '>
-        <div className='lg:w-1/6 bg-gray-400 w-full h-screen'>
-          <h1></h1>
+      <div className='w-full  h-full flex items-center lg:justify-start py-10  justify-center flex-wrap  '>
+        <div className='lg:w-1/6 bg-gray-300 w-full h-screen py-20 '>
+          <h1 className=' items-center justify-center md:text-2xl text-1xl sm:text-1xl font-extrabold mb-4 text-black items-center ' >Job Categories</h1>
+          <ul>
+            <li>
+              <dv className="pr-20 mr-20 w-full">
+                <button onClick={() => setIsOpen2((prev)=> !prev)} className='items-center justify-center w-full jus py-2 my-2 sm:my-0 border bord-white-600 rounded uppercase tracking-widest mx-0   text-white bg-gray-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-gray-600 flex '>Architect & Civil Engineering
+                {!isOpen2 ?(
+                  <AiOutlineCaretDown className='h-8'/> 
+                  ):(
+                  <AiOutlineCaretUp className='h-8'/>
+                )}
+                </button>
+                {isOpen2 && (
+                <div className='bg-gray-500 absolute items-start rounded-lg py-1 my-1 w-1/6'>
+                  <ul >
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Architects</Link></li>
+                    <li className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Surveyor</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Quantity Surveyor</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Civil Eng Trainee & internship</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Desing Engineer</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Interior Designers</Link></li>
+                  </ul>
+                </div>
+              )}
+            </dv></li>
+            <li>
+              <dv className="pr-20 mr-20 w-full">
+                <button onClick={() => setIsOpen6((prev)=> !prev)} className='items-center justify-center w-full jus py-2 my-2 sm:my-0 border bord-white-600 rounded uppercase tracking-widest mx-0   text-white bg-gray-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-gray-600 flex '>Computer Engineering & Technology
+                {!isOpen6 ?(
+                  <AiOutlineCaretDown className='h-8'/> 
+                  ):(
+                  <AiOutlineCaretUp className='h-8'/>
+                )}
+                </button>
+                {isOpen6 && (
+                <div className='bg-gray-400 absolute items-start rounded-lg py-1 my-1 w-1/6'>
+                  <ul >
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Software Engineers</Link></li>
+                    <li className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Mobile App Developer</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Business Analyst</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Full Stack Development</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Data Analyst</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Ui/Ux Designer</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Software cloud Engineer</Link></li>
+                  </ul>
+                </div>
+              )}
+            </dv></li>
+            <li>
+              <dv className="pr-20 mr-20 w-full">
+                <button onClick={() => setIsOpen3((prev)=> !prev)} className='items-center justify-center w-full jus py-2 my-2 sm:my-0 border bord-white-600 rounded uppercase tracking-widest mx-0   text-white bg-gray-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-gray-600 flex '>Digital Marketing & Custent Writing
+                {!isOpen3 ?(
+                  <AiOutlineCaretDown className='h-8'/> 
+                  ):(
+                  <AiOutlineCaretUp className='h-8'/>
+                )}
+                </button>
+                {isOpen3 && (
+                <div className='bg-gray-500 absolute items-start rounded-lg py-1 my-1 w-1/6'>
+                  <ul >
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Digital Marketing</Link></li>
+                    <li className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Web Designing</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Social Media, SEO $ Content Writing</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Graphic Designing</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Any Designing</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Trainee Graphic Desingner</Link></li>
+                  </ul>
+                </div>
+              )}
+            </dv></li>
+            <li>
+              <dv className="pr-20 mr-20 w-full">
+                <button onClick={() => setIsOpen4((prev)=> !prev)} className='items-center justify-center w-full jus py-2 my-2 sm:my-0 border bord-white-600 rounded uppercase tracking-widest mx-0   text-white bg-gray-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-gray-600 flex '>Architect & Civil Engineering
+                {!isOpen4 ?(
+                  <AiOutlineCaretDown className='h-8'/> 
+                  ):(
+                  <AiOutlineCaretUp className='h-8'/>
+                )}
+                </button>
+                {isOpen4 && (
+                <div className='bg-gray-400 absolute items-start rounded-lg py-1 my-1 w-1/6'>
+                  <ul >
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Architects</Link></li>
+                    <li className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Surveyor</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Quantity Surveyor</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Civil Eng Trainee & internship</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Desing Engineer</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Interior Designers</Link></li>
+                  </ul>
+                </div>
+              )}
+            </dv></li>
+            <li>
+              <dv className="pr-20 mr-20 w-full">
+                <button onClick={() => setIsOpen5((prev)=> !prev)} className='items-center justify-center w-full jus py-2 my-2 sm:my-0 border bord-white-600 rounded uppercase tracking-widest mx-0   text-white bg-gray-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-gray-600 flex '>Architect & Civil Engineering
+                {!isOpen5 ?(
+                  <AiOutlineCaretDown className='h-8'/> 
+                  ):(
+                  <AiOutlineCaretUp className='h-8'/>
+                )}
+                </button>
+                {isOpen5 && (
+                <div className='bg-gray-400 absolute items-start rounded-lg py-1 my-1 w-1/6'>
+                  <ul >
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Architects</Link></li>
+                    <li className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Surveyor</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Quantity Surveyor</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Civil Eng Trainee & internship</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Desing Engineer</Link></li>
+                    <li onClick={handleSearch} className='flex w-full justify-between pl-10 hover:bg-gray-600 cursor-pointer rounded-r-lg border-l-transparent'><Link href="/auth/login" className="font-medium text-white hover:underline ">Interior Designers</Link></li>
+                  </ul>
+                </div>
+              )}
+            </dv></li>
+          </ul>
         </div> 
         <div className='lg:w-4/6 w-full sm:p-2 h-full my-2 flex items-center justify-center px-4 md:items-start md:justify-start md:p-5 flex-col '>
           <h1 className=' items-center justify-center md:text-6xl text-2xl sm:text-2xl font-extrabold mb-4 text-black items-center '>The Best Place to Find Your <span className='text-indigo-600 justify-center flex items-center  '> Dream Jobs.</span> </h1>
